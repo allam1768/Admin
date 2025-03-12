8cac9a6e8562fab5f6e494fa5eadb1aac093a544
alamp <159858484+allam1768@users.noreply.github.com>
2025-03-12 22:58:44 +0700

    feat [AP]:slicing data client screen

---
+++ lib/app/pages/Data Client Screen/data_client_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class DataClientController extends GetxController {
+  final clients = <Map<String, String>>[
+    {"company": "Indofood", "client": "Wawan", "imagePath": "assets/images/example.png"},
+    {"company": "Unilever", "client": "Doni", "imagePath": "assets/images/example.png"},
+    {"company": "Nestle", "client": "Rina", "imagePath": "assets/images/example.png"},
+  ].obs;
+}
--- lib/app/pages/Data Client Screen/data_client_view.dart
+++ lib/app/pages/Data Client Screen/data_client_view.dart
@@ -1,15 +1,50 @@
-  import 'package:flutter/cupertino.dart';
+import 'package:admin/app/pages/Data%20Client%20Screen/data_client_controller.dart';
+import 'package:admin/app/pages/Data%20Client%20Screen/widgets/ClientCard.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import '../../global component/app_bar.dart';
 
-  class DataClientView extends StatefulWidget {
-    const DataClientView({super.key});
+class DataClientView extends StatelessWidget {
+  const DataClientView({super.key});
 
-    @override
-    State<DataClientView> createState() => _DataClientViewState();
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(DataClientController());
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFCCD7CD),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomAppBar(
+              title: "Client",
+              rightIcon: "assets/icons/add_client_btn.svg",
+              rightOnTap: () {
+                Get.offNamed('/add-client'); // Ganti dengan rute yang benar
+              },
+              showBackButton: false,
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 35.w, vertical: 20.h),
+                child: Obx(() => ListView.separated(
+                  itemCount: controller.clients.length,
+                  separatorBuilder: (context, index) => SizedBox(height: 20.h),
+                  itemBuilder: (context, index) {
+                    return ClientCard(
+                      company: controller.clients[index]["company"]!,
+                      client: controller.clients[index]["client"]!,
+                      imagePath: controller.clients[index]["imagePath"]!,
+                    );
+                  },
+                )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
-  class _DataClientViewState extends State<DataClientView> {
-    @override
-    Widget build(BuildContext context) {
-      return const Placeholder();
-    }
-  }
+}
