82762317bc5f8adfab72b43fe1983f2eb81bcac1
alamp <159858484+allam1768@users.noreply.github.com>
2025-03-12 23:32:58 +0700

    feat [AP]:slicing data worker screen

--- lib/app/pages/Data Worker Screen/data_worker_view.dart
+++ lib/app/pages/Data Worker Screen/data_worker_view.dart
@@ -1,15 +1,51 @@
-import 'package:flutter/cupertino.dart';
+import 'package:admin/app/pages/Data%20Client%20Screen/widgets/ClientCard.dart';
+import 'package:admin/app/pages/Data%20Worker%20Screen/widgets/WorkerCard.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import '../../global component/app_bar.dart';
+import 'data_worker_controller.dart';
 
-class DataWorkerView extends StatefulWidget {
+class DataWorkerView extends StatelessWidget {
   const DataWorkerView({super.key});
 
   @override
-  State<DataWorkerView> createState() => _DataWorkerViewState();
-}
-
-class _DataWorkerViewState extends State<DataWorkerView> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final controller = Get.put(DataWorkerController());
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFCCD7CD),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomAppBar(
+              title: "Worker",
+              rightIcon: "assets/icons/add_worker_btn.svg",
+              rightOnTap: () {
+                Get.offNamed('/add-client');
+              },
+              showBackButton: false,
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 35.w, vertical: 20.h),
+                child: Obx(() => ListView.separated(
+                  itemCount: controller.worker.length,
+                  separatorBuilder: (context, index) => SizedBox(height: 20.h),
+                  itemBuilder: (context, index) {
+                    return WorkerCard(
+                      nokaryawan: controller.worker[index]["nokaryawan"]!,
+                      name: controller.worker[index]["nama"]!,
+                      imagePath: controller.worker[index]["imagePath"]!,
+                    );
+                  },
+                )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
