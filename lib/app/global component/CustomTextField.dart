ee542f0606e12336ad158a040429fbc9c3762eac
alamp <159858484+allam1768@users.noreply.github.com>
2025-03-28 09:11:22 +0700

    fead [AP]:remove splash flutter

--- lib/app/global component/CustomTextField.dart
+++ lib/app/global component/CustomTextField.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class CustomTextField extends StatefulWidget {
+class CustomTextField extends StatelessWidget {
   final String label;
   final bool isPassword;
   final bool isNumber;
@@ -19,56 +19,54 @@ class CustomTextField extends StatefulWidget {
   });
 
   @override
-  _CustomTextFieldState createState() => _CustomTextFieldState();
-}
-
-class _CustomTextFieldState extends State<CustomTextField> {
-  bool _isObscured = true;
-
-  @override
   Widget build(BuildContext context) {
+    final ValueNotifier<bool> isObscured = ValueNotifier<bool>(true);
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          widget.label,
+          label,
           style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.bold),
         ),
         SizedBox(height: 4.h),
-        TextField(
-          obscureText: widget.isPassword ? _isObscured : false,
-          maxLines: widget.isPassword ? 1 : null,
-          keyboardType: widget.isNumber ? TextInputType.number : TextInputType.text,
-          style: TextStyle(fontSize: 15.sp),
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.white,
-            prefixIcon: widget.svgIcon != null
-                ? Padding(
-              padding: EdgeInsets.symmetric(horizontal: 12.w),
-              child: SvgPicture.asset(widget.svgIcon!, width: 20.r, height: 20.r),
-            )
-                : null,
-            suffixIcon: widget.isPassword
-                ? IconButton(
-              icon: SvgPicture.asset(
-                _isObscured ? 'assets/icons/eye_closed.svg' : 'assets/icons/eye_open.svg',
-                width: 20.r,
-                height: 20.r,
+        ValueListenableBuilder<bool>(
+          valueListenable: isObscured,
+          builder: (context, obscured, _) {
+            return TextField(
+              obscureText: isPassword ? obscured : false,
+              maxLines: isPassword ? 1 : null,
+              keyboardType: isNumber ? TextInputType.number : TextInputType.text,
+              style: TextStyle(fontSize: 15.sp),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                prefixIcon: svgIcon != null
+                    ? Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12.w),
+                  child: SvgPicture.asset(svgIcon!, width: 20.r, height: 20.r),
+                )
+                    : null,
+                suffixIcon: isPassword
+                    ? IconButton(
+                  icon: SvgPicture.asset(
+                    obscured ? 'assets/icons/eye_closed.svg' : 'assets/icons/eye_open.svg',
+                    width: 20.r,
+                    height: 20.r,
+                  ),
+                  onPressed: () {
+                    isObscured.value = !obscured;
+                  },
+                )
+                    : null,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.r),
+                  borderSide: BorderSide.none,
+                ),
               ),
-              onPressed: () {
-                setState(() {
-                  _isObscured = !_isObscured;
-                });
-              },
-            )
-                : null,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8.r),
-              borderSide: BorderSide.none,
-            ),
-          ),
-          onChanged: widget.onChanged,
+              onChanged: onChanged,
+            );
+          },
         ),
       ],
     );
